bc6a6eacc910d36c9dbaf1aeb8d30a1d693bd82a
skoanton <antonhakanssonn@gmail.com>
2024-04-29 16:12:38 +0200

    navbar component added

--- src/pages/Root/Root.tsx
+++ src/pages/Root/Root.tsx
@@ -1,12 +1,13 @@
 import { Outlet } from "react-router-dom";
+import Navbar from "./components/Navbar";
 
 type RootProps = {};
 
 const Root = ({}: RootProps) => {
   return (
     <>
-      <div>Root</div>
       <Outlet />
+      <Navbar />
     </>
   );
 };
---
+++ src/pages/Root/components/Navbar.tsx
@@ -0,0 +1,57 @@
+import { Button } from "@/components/ui/button";
+
+import { Home, Library, Search } from "lucide-react";
+import { Link, useLocation } from "react-router-dom";
+
+type NavbarProps = {};
+
+const Navbar = ({}: NavbarProps) => {
+  const location = useLocation();
+  return (
+    <header className="w-full bg-primary p-4 fixed bottom-0 left-0">
+      <ul className="flex justify-between">
+        <li>
+          <Link to="/home">
+            <Button
+              className={
+                location.pathname === "/home"
+                  ? "text-muted-foreground"
+                  : "text-primary-foreground"
+              }
+            >
+              <Home className="w-12 h-12" />
+            </Button>
+          </Link>
+        </li>
+        <li>
+          <Link to="/search">
+            <Button
+              className={
+                location.pathname === "/search"
+                  ? "text-muted-foreground"
+                  : "text-primary-foreground"
+              }
+            >
+              <Search className="w-12 h-12" />
+            </Button>
+          </Link>
+        </li>
+        <li>
+          <Link to="/shelf/favorites">
+            <Button
+              className={
+                location.pathname.startsWith("/shelf/")
+                  ? "text-muted-foreground"
+                  : "text-primary-foreground"
+              }
+            >
+              <Library className="w-12 h-12" />
+            </Button>
+          </Link>
+        </li>
+      </ul>
+    </header>
+  );
+};
+
+export default Navbar;
