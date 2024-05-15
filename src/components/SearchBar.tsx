43b2611116feb5c5fb02d368164b05abfccef84f
skoanton <antonhakanssonn@gmail.com>
2024-05-15 09:36:48 +0200

    la till radiobox

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -11,6 +11,7 @@ import { useNavigate } from "react-router-dom";
 import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
+import { useState } from "react";
 
 const searchSchema = z.object({
   searchString: z.string().min(1, "Please type a word first"),
@@ -18,7 +19,7 @@ const searchSchema = z.object({
 
 const SearchBar = () => {
   const navigate = useNavigate();
-
+  const [isSearchTitle, setIsSearchTitle] = useState(true);
   const searchForm = useForm<z.infer<typeof searchSchema>>({
     resolver: zodResolver(searchSchema),
     mode: "onSubmit",
@@ -34,6 +35,14 @@ const SearchBar = () => {
   return (
     <main className="h-[calc(100vh-10rem)] flex items-center">
       <section className="w-full">
+        <section className="flex items-center justify-center gap-4">
+          <label className="text-primary-foreground" htmlFor="searchType">
+            Title
+          </label>
+          <input type="radio" name="searchType" checked={isSearchTitle} />
+          <label className="text-primary-foreground">Author</label>
+          <input type="radio" name="searchType" checked={!isSearchTitle} />
+        </section>
         <Form {...searchForm}>
           <form
             onSubmit={searchForm.handleSubmit(handleSearch)}
